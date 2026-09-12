1a77ef6a3ceca1b0e32649444aceedd24fd7e19d
unknown <rojleenapegu05@gmail.com>
2026-09-13 01:30:40 +0530

    correction in 1.b

--- iqb1.py
+++ iqb1.py
@@ -42,10 +42,10 @@ print(score_table)
 #doing 1.b: finding the maximum score and its position
 max_score = np.max(score_matrix)
 
-max_positions = np.argwhere(score_matrix == max_score)
+max_pos = np.argwhere(score_matrix == max_score)
 
 print("\nb. Maximum score:", max_score)
-print("Positions:", [tuple(pos) for pos in max_positions])
+print("Positions:", [tuple(pos) for pos in max_pos])
 
 #doing 1.c: tracing back from the max score position until we reach a score of 0
 def traceback(max_row, max_col):
